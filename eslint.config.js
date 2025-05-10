c51a8d082aa40db8eefa35c10f8467e4e4b27bde
Hamza Aamir <hamzaaamir6@gmail.com>
2025-05-10 23:30:44 +0500

    frontend app general settings added

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,30 @@
-// const nx = require('@nx/eslint-plugin');
+const nx = require('@nx/eslint-plugin');
 
-// module.exports = [
-//   ...nx.configs['flat/base'],
-//   ...nx.configs['flat/typescript'],
-//   ...nx.configs['flat/javascript'],
-//   {
-//     ignores: ['**/dist'],
-//   },
-//   {
-//     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-//     rules: {
-//       '@nx/enforce-module-boundaries': [
-//         'error',
-//         {
-//           enforceBuildableLibDependency: true,
-//           allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
-//           depConstraints: [
-//             {
-//               sourceTag: '*',
-//               onlyDependOnLibsWithTags: ['*'],
-//             },
-//           ],
-//         },
-//       ],
-//       // 👇 Add this line to disable the rule
-//       '@typescript-eslint/no-explicit-any': 'off',
-//     },
-//   },
-// ];
+module.exports = [
+  ...nx.configs['flat/base'],
+  ...nx.configs['flat/typescript'],
+  ...nx.configs['flat/javascript'],
+  {
+    ignores: ['**/dist'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
+          depConstraints: [
+            {
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+          ],
+        },
+      ],
+      // 👇 Add this line to disable the rule
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+];
